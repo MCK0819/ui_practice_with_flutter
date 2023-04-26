a9223ac875ecf3c3d603ac91a59d4213aa2fdaa4
MCK <mck0819@gmail.com>
2023-04-26 14:09:31 +0900

    statefulWidget 튜토리얼

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,53 @@ import 'package:ui_practice_with_flutter/widgets/button.dart';
 import 'package:ui_practice_with_flutter/widgets/currency_card.dart';
 
 void main() {
-  runApp(const MyApp());
+  //runApp(const MyApp());
+  runApp(const MyWidget());
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  int counter = 0;
+
+  void onClicked() {
+    setState(() {
+      counter = counter + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(fontSize: 30),
+              ),
+              IconButton(
+                  iconSize: 40,
+                  onPressed: onClicked,
+                  icon: const Icon(Icons.add_box_rounded))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -13,26 +59,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF181818),
+        backgroundColor: const Color(0xFF181818),
         body: SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsets.symmetric(
+            padding: const EdgeInsets.symmetric(
               horizontal: 20,
             ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
+                const SizedBox(
                   height: 30,
                 ),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.
-                  end,
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
-                        Text(
+                        const Text(
                           'Hello, Leo',
                           style: TextStyle(
                             color: Colors.white,
@@ -47,50 +92,62 @@ class MyApp extends StatelessWidget {
                             fontSize: 18,
                           ),
                         ),
-                      ],)
+                      ],
+                    )
                   ],
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 50,
                 ),
-                Text('Total Balance',
-                  style: TextStyle(color: Colors.white.withOpacity(0.8),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 5,
                 ),
-                Text('\$5 194 482',
-                  style: TextStyle(color: Colors.white,
+                const Text(
+                  '\$5 194 482',
+                  style: TextStyle(
+                      color: Colors.white,
                       fontSize: 48,
-                      fontWeight: FontWeight.w600
-                  ),
+                      fontWeight: FontWeight.w600),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 30,
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Button(text: 'Transfer', bgColor: Color(0xFFF1B33B), textColor: Colors.black),
-                    Button(text: 'request', bgColor: Color(0xFF1F2123), textColor: Colors.white),
+                    Button(
+                        text: 'Transfer',
+                        bgColor: const Color(0xFFF1B33B),
+                        textColor: Colors.black),
+                    Button(
+                        text: 'request',
+                        bgColor: const Color(0xFF1F2123),
+                        textColor: Colors.white),
                   ],
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 100,
                 ),
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('Wallets',
+                    const Text(
+                      'Wallets',
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 36,
                         fontWeight: FontWeight.w600,
                       ),
                     ),
-                    Text('View All',
+                    Text(
+                      'View All',
                       style: TextStyle(
                         color: Colors.white.withOpacity(0.8),
                         fontSize: 18,
@@ -98,18 +155,20 @@ class MyApp extends StatelessWidget {
                     ),
                   ],
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 20,
                 ),
-                CurrencyCard(name: 'Euro',
+                const CurrencyCard(
+                  name: 'Euro',
                   code: 'EUR',
                   amount: '6 428',
                   icon: Icons.euro_rounded,
                   isInverted: false,
                 ),
                 Transform.translate(
-                  offset: Offset(0,-20),
-                  child: CurrencyCard(name: 'Dollar',
+                  offset: const Offset(0, -20),
+                  child: const CurrencyCard(
+                    name: 'Dollar',
                     code: 'USD',
                     amount: '428',
                     icon: Icons.attach_money,
@@ -117,8 +176,9 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
                 Transform.translate(
-                  offset: Offset(0,-40),
-                  child: CurrencyCard(name: 'Bitcoin',
+                  offset: const Offset(0, -40),
+                  child: const CurrencyCard(
+                    name: 'Bitcoin',
                     code: 'BTC',
                     amount: '9 785',
                     icon: Icons.currency_bitcoin,
